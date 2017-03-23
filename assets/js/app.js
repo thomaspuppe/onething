dc5606a14be4c3acd140718f7f58bb3b585821ed
Thomas Puppe <info@thomaspuppe.de>
2017-03-23 22:25:35 +0100

    store items in localstorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,28 @@
 (function () {
-	var data = {
-		items: [],
-		newItem: ''
+
+	var STORAGE_KEY = 'todos-vuejs';
+
+	var storage = {
+		fetch: function () {
+			return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+		},
+		save: function (items) {
+			localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
+		}
 	};
+
 	new Vue({
 		el: '#app',
-		data: data,
+		data: {
+			items: storage.fetch(),
+			newItem: ''
+		},
+		watch: {
+			items: {
+				deep: true,
+				handler: storage.save
+			}
+		},
 		methods: {
 			addItem: function () {
 				var text = this.newItem.trim();
